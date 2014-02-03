bda61619014d90123597ab7a6224b6dc0099d90f
unknown <admin@webpagebytes.com>
2014-02-03 23:52:38 +0200

    refactor the change project locales and languages page

--- src/main/resources/META-INF/admin/app-js/wbsettingslangsedit.js
+++ src/main/resources/META-INF/admin/app-js/wbsettingslangsedit.js
@@ -4,102 +4,128 @@ var errorsGeneral = {
 $().ready( function () {
 	
 	var displayHandler = function (fieldId, record) {
-		if (fieldId=="enable_") {
-				return "<input type='checkbox' id='enable_" + record['lcid'] + "'>";
-			} 
-		if (fieldId=="default_") {
-				return "<input type='checkbox' id='default_" + record['lcid'] + "'>";
-			} 	
+		if (fieldId == 'default') {
+			if (record[fieldId] == "1") {
+				return '<input type="checkbox" class="lang_default" data-code="{0}" checked>'.format(escapehtml(record['code']));
+			} else
+			{
+				return '<input type="checkbox" class="lang_default" data-code="{0}">'.format(escapehtml(record['code']));		
+			}
+		}
+
+		return "";
 	};
-				
-	$('#wblangstablediv').wbTable( { columns: [ {display: "Enable language", fieldId:"enable_", customHandling: true, customHandler: displayHandler}, 
-												{display: "Language name", fieldId:"name"}, 
-												{display: "Language code", fieldId:"lcid"}, 
-												{display: "Default language", fieldId: "default_", customHandling: true, customHandler: displayHandler} ],
-						 keyName: "lcid",
+	$('#wbSelectedLanguages').wbSimpleTable( { columns: [ {display: "name", fieldId:"name"}, {display: "Code", fieldId:"code"}, {display: "Default language", fieldId: "default", customHandler: displayHandler} ],
+						 keyName: "code",
 						 tableBaseClass: "table table-condensed table-color-header",
 						 paginationBaseClass: "pagination",
-						 itemsPerPage: 1000
+						 itemsPerPage: 1000,
+						 noLinesContent: "<tr> <td colspan='3'>There are no site urls defined. </td></tr>"
 						});
+						
+	$(".letter").click ( function (e) {
+		e.preventDefault();
+		$(".langs_group").hide();
+		var letterClass = "langs_" + $.trim(e.target.text);
+		$("." + letterClass).show();
+	});	
+	$("#wbSelectedLanguages").on("click", ".lang_default", function (e) {		
+			var code = $(e.target).attr('data-code');
+			var record = $('#wbSelectedLanguages').wbSimpleTable().getRowDataWithKey(code);
+			var prevValue = record['default'];
+			var allRecords = $('#wbSelectedLanguages').wbSimpleTable().getAllRowsData();
+			for (var index in allRecords) {
+				allRecords[index]['default'] = "0";
+			}
+			$('#wbSelectedLanguages').wbSimpleTable().setRows(allRecords);
+			if (prevValue == "0") {
+				record['default'] = "1";
+			} else
+			{
+				record['default'] = "0";
+			}
+			$('#wbSelectedLanguages').wbSimpleTable().updateRowWithKey(record, code);
 
-	$('#wblangstablediv').wbCommunicationManager();
+	});
+	
+	$(".letter_A").trigger("click");
+	
+	var clickEnableHandler = function(elem, isDefault){
+		var tds = $(elem).parent().siblings();
+		if (tds.length == 2) {
+			var name = $(tds[0]).text();
+			var code = $(tds[1]).text();
+			if ($(elem).is(':checked')) {
+				if (isDefault) {
+					isDefault = "1";
+				} else {
+					isDefault = "0"
+				}
+				var record = {'name': name, 'code':code, 'default':isDefault };
+				$('#wbSelectedLanguages').wbSimpleTable().insertRow(record);											
+			} else {
+				$('#wbSelectedLanguages').wbSimpleTable().deleteRowWithKey(code);
+			}
+			
+		}		
+	}
+	$("input[id^='enable_']").click( function (e) {
+		clickEnableHandler(e.target, false);
+	});
 	
 	var fSuccessSupportedLanguages = function (data) {
 		$.each(data.data, function(index, item) {
-			var idenable = '#enable_' + item['lcid'];
-			$(idenable).prop('checked', true);
-			if (item['default'] == 'true') {
-				var iddefault = '#default_' + item['lcid'];
-				$(iddefault).prop('checked', true);
-			}			
+			var idenable = 'enable_' + item['lcid'];
+			var elem = $("#"+idenable).prop("checked", true);
+			clickEnableHandler(elem, item['default']=="true");
 		});
 	};
 	var fErrorSupportedLanguages = function (data) {
 		alert(data);
 	};	
-	var fSuccessGetLanguages = function (data) {
-		$.each(data.data, function(index, item) {
-			$('#wblangstablediv').wbTable().insertRow(item);
-		});				
-		$('#wblangstablediv').wbCommunicationManager().ajax ( { url:"./wbsupportedlanguages",
-													 httpOperation:"GET", 
-													 payloadData:"",
-													 functionSuccess: fSuccessSupportedLanguages,
-													 functionError: fErrorSupportedLanguages
-													} );
-		
-	};
-	var fErrorGetLanguages = function (errors, data) {
-		alert(data);
-	};
+
 	
-	$('#wblangstablediv').wbCommunicationManager().ajax ( { url:"./wblanguages",
-													 httpOperation:"GET", 
-													 payloadData:"",
-													 functionSuccess: fSuccessGetLanguages,
-													 functionError: fErrorGetLanguages
-													} );
+	$('#wblangcatalog').wbCommunicationManager();
+	$('#wblangcatalog').wbCommunicationManager().ajax ( { url:"./wbsupportedlanguages",
+		 httpOperation:"GET", 
+		 payloadData:"",
+		 functionSuccess: fSuccessSupportedLanguages,
+		 functionError: fErrorSupportedLanguages
+		} );
 
-	var fSuccessSetSupportedLanguages = function(data) {
-		window.location.href = './websettingslangs.html';
-	};
+	var fSuccessSetSupportedLanguages = function(data){
+		window.location.href="./websettingslangs.html";
+	}
+	var fErrorSetSupportedLanguages = function(data){
+		
+	}
 
-	var fErrorSetSupportedLanguages = function(data) {
-		alert(data);
-	};
-	$('.wbCancelLanguagesBtnClass').click ( function (e) {
+	$(".wbSubmitSupportedLanguages").click( function (e) {
 		e.preventDefault();
-		window.location.href = './websettingslangs.html';
+		var allRecords = $('#wbSelectedLanguages').wbSimpleTable().getAllRowsData();
+		var array = new Array();
+		var hasDefault = false;
+		for (var index in allRecords) {
+			var item = {};
+			item['lcid'] = allRecords[index]['code'];
+			if (allRecords[index]['default'] == "1") {
+				item['default'] = 'true';
+			} else {
+				item['default'] = 'false';
+			}
+			array.push(item);
+		}
+		var payload = JSON.stringify(array);
+	
+		$('#wblangstablediv').wbCommunicationManager().ajax ( { url:"./wbsupportedlanguages",
+			 httpOperation:"PUT", 
+			 payloadData: payload,
+			 functionSuccess: fSuccessSetSupportedLanguages,
+			 functionError: fErrorSetSupportedLanguages
+			} );
+
+		
 	});
 	
-	$('.wbEditLanguagesBtnClass').click ( function (e) {
-		e.preventDefault();
-		var langs = new Array();
-		var elements = $('input[id^=enable_]');
-		$.each (elements, function(index, elem) {
-			if ($(elem).is(":checked")) {
-				var id = $(elem).attr('id').substr('enable_'.length);		
-				var langItem = {};
-				langItem['lcid'] = id;
-				
-				var iddefault = 'default_' + id;
-				if ($("#" + iddefault).is(":checked")) {
-					langItem['default'] = 'true';
-				} else {
-					langItem['default'] = 'false';
-				}
-				langs.push(langItem);
-			}
-		});
-		var payload = JSON.stringify(langs);
-		
-		$('#wblangstablediv').wbCommunicationManager().ajax ( { url:"./wbsupportedlanguages",
-											 httpOperation:"PUT", 
-											 payloadData: payload,
-											 functionSuccess: fSuccessSetSupportedLanguages,
-											 functionError: fErrorSetSupportedLanguages
-											} );
 
-		
-	});	
 });
