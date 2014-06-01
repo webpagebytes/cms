e8e820efda47f53b7ac19062ec8d96fd2a7f17d3
wpb <admin@webpagebytes.com>
2014-06-01 23:07:07 +0300

    remove gae dependencies

--- src/test/java/com/webpagebytes/cms/datautility/TestWBLocalCloudFileStorage.java
+++ src/test/java/com/webpagebytes/cms/datautility/TestWBLocalCloudFileStorage.java
@@ -2,18 +2,16 @@ package com.webpagebytes.cms.datautility;
 
 import static org.junit.Assert.*;
 
+
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.hsqldb.lib.FileUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
