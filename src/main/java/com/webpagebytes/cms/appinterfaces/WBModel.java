a8c96db2a3a04cb8ff0b6a554bb3d17a870aa154
wpb <admin@webpagebytes.com>
2014-06-06 01:14:27 +0300

    rename freemarker predefined model variables of webpagebytes to start with wpb

--- src/main/java/com/webpagebytes/cms/appinterfaces/WBModel.java
+++ src/main/java/com/webpagebytes/cms/appinterfaces/WBModel.java
@@ -5,28 +5,28 @@ import java.util.Set;
 
 public class WBModel {
 	
-	public static final String GLOBALS_KEY = "wbGlobals";
-	public static final String REQUEST_KEY = "wbRequest";
-	public static final String PAGE_PARAMETERS_KEY = "wbPageParams";
-	public static final String URI_PARAMETERS_KEY = "wbUriParams";
+	public static final String GLOBALS_KEY = "wpbGlobals";
+	public static final String REQUEST_KEY = "wpbRequest";
+	public static final String PAGE_PARAMETERS_KEY = "wpbPageParams";
+	public static final String URI_PARAMETERS_KEY = "wpbUriParams";
 	
-	public static final String PAGE_CONTROLLER_MODEL_KEY = "wbPageModel";
-	public static final String URI_CONTROLLER_MODEL_KEY = "wbUriModel";
+	public static final String PAGE_CONTROLLER_MODEL_KEY = "wpbPageModel";
+	public static final String URI_CONTROLLER_MODEL_KEY = "wpbUriModel";
 	
-	public static final String LOCALE_KEY = "wbLocale";
-	public static final String LOCALE_LANGUAGE_KEY = "wbLocaleLanguage";
-	public static final String LOCALE_COUNTRY_KEY = "wbLocaleCountry";
-	public static final String LOCALE_MESSAGES = "wbMessages";
-	public static final String MODULE_DIRECTIVE = "wbModule";
-	public static final String IMAGE_DIRECTIVE = "wbImage";
-	public static final String ARTICLE_DIRECTIVE = "wbArticle";
-	public static final String TEXT_FORMAT_DIRECTIVE = "wbFormatText";
-	public static final String FORMAT_TEXT_METHOD = "wbFormatText";
+	public static final String LOCALE_KEY = "wpbLocale";
+	public static final String LOCALE_LANGUAGE_KEY = "wpbLocaleLanguage";
+	public static final String LOCALE_COUNTRY_KEY = "wpbLocaleCountry";
+	public static final String LOCALE_MESSAGES = "wpbMessages";
+	public static final String MODULE_DIRECTIVE = "wpbModule";
+	public static final String IMAGE_DIRECTIVE = "wpbImage";
+	public static final String ARTICLE_DIRECTIVE = "wpbArticle";
+	public static final String TEXT_FORMAT_DIRECTIVE = "wpbFormatText";
+	public static final String FORMAT_TEXT_METHOD = "wpbFormatText";
 	
 	
-	public static final String GLOBAL_PROTOCOL = "WB_GLOBAL_PROTOCOL";
-	public static final String GLOBAL_DOMAIN = "WB_GLOBAL_DOMAIN";
-	public static final String GLOBAL_URI_PREFIX = "WB_GLOBAL_URI_PREFIX";
+	public static final String GLOBAL_PROTOCOL = "WPB_GLOBAL_PROTOCOL";
+	public static final String GLOBAL_DOMAIN = "WPB_GLOBAL_DOMAIN";
+	public static final String GLOBAL_URI_PREFIX = "WPB_GLOBAL_URI_PREFIX";
 
 	protected WBCmsModel cmsModel = new WBCmsModel();
 	protected WBCustomModel customModel = new WBCustomModel();
