677cd317b0b6df74c682c059ce1bc792aebc4ca3
wpb <admin@webpagebytes.com>
2014-11-28 20:18:07 +0200

    add license header to files

--- src/main/java/com/webpagebytes/cms/template/FreeMarkerTemplateLoader.java
+++ src/main/java/com/webpagebytes/cms/template/FreeMarkerTemplateLoader.java
@@ -1,13 +1,25 @@
+/*
+ *   Copyright 2014 Webpagebytes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package com.webpagebytes.cms.template;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-
-import com.webpagebytes.cms.cache.WPBCacheFactory;
 import com.webpagebytes.cms.cache.WPBCacheInstances;
-import com.webpagebytes.cms.cache.WPBWebPageModulesCache;
-import com.webpagebytes.cms.cache.WPBWebPagesCache;
 import com.webpagebytes.cms.cmsdata.WPBWebPage;
 import com.webpagebytes.cms.cmsdata.WPBWebPageModule;
 import com.webpagebytes.cms.exception.WPBIOException;
