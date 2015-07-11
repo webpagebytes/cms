cd92efd927a996b3912aaf2d089a8e20681b0762
webpagebytes <admin@webpagebytes.com>
2015-07-11 16:29:17 +0300

    add java doc for new interfaces (WPBAuthentication and WPBAuthenticationResult

--- src/main/java/com/webpagebytes/cms/WPBAuthentication.java
+++ src/main/java/com/webpagebytes/cms/WPBAuthentication.java
@@ -23,6 +23,11 @@ import javax.servlet.http.HttpServletRequest;
 import com.webpagebytes.cms.exception.WPBException;
 import com.webpagebytes.cms.exception.WPBIOException;
 
+/**
+ * Interface that authenticates requests to the administration APIs of Webpagebytes CMS.
+ * The interface completely abstracts how the authentication is implemented, can be either an application specific implementation
+ * or it can use a 3rd party authentication solution (i.e. Google, Yahoo, Facebook,...).
+ */
 public interface WPBAuthentication {
 	public static final String CONFIG_LOGIN_REDIRECT = "loginRedirect";
 	public static final String CONFIG_LOGIN_PAGE_URL = "loginPageUrl";
@@ -30,6 +35,23 @@ public interface WPBAuthentication {
 	public static final String CONFIG_PROFILE_URL = "profileUrl";	
 	public static final String CONFIG_USER_IDENTIFIER = "userIdentifier";
 	
+	/**
+	 * Initializes the authentication with parameters from the CMS configuration xml file that corresponds to wpbauthentication section.
+	 * @param params Map of keys and values representing configuration parameters
+	 * @throws WPBIOException Exception
+	 */
 	public void initialize(Map<String, String> params) throws WPBException;
+	
+	/**
+	 * Method that checks if a request is authenticated. A typical implementation will use a HTTP session cookie to track and
+	 * validate the request(s).
+	 * @param request The HTTP request that needs to be authenticated
+	 * @return A WPBAuthenticationResult instance. If the userIdentifier member of WPBAuthenticationResult instance that is returned is 
+	 * 		   not set then the request will be interpreted by the CMS engine as not authenticated, in this case the web browser will be 
+	 * 		   redirected to the loginLink member value of the same instance. <br>
+	 *         If the return value is null then the CMS engine will treat the request as authenticated.
+	 *         The CMS administration interface will display the authenticated user as the value of the userIdentifier member of the returned instance.
+	 * @throws WPBIOException Exception
+	 */
 	public WPBAuthenticationResult checkAuthentication(HttpServletRequest request) throws WPBIOException;
 }
--- src/main/java/com/webpagebytes/cms/WPBAuthenticationResult.java
+++ src/main/java/com/webpagebytes/cms/WPBAuthenticationResult.java
@@ -16,11 +16,33 @@
 
 package com.webpagebytes.cms;
 
+/**
+ * Interface that represents the authentication result of checkAuthentication call from WPBAuthentication interface.
+ */
 public interface WPBAuthenticationResult {
-		
+	/**
+	 * Returns the string representation of the authenticated user. For example his email address.
+	 * This value will be displayed in the CMS administration web interface as the authenticated user.
+	 * @return Authenticated user identifier or null/empty string for requests that are not authenticated.
+	 */
 	public String getUserIdentifier();
+	
+	/**
+	 * Returns the logout link that will be displayed in the CMS administration web interface.
+	 * @return Logout link url
+	 */
 	public String getLogoutLink();
+
+	/**
+	 * Returns the login page link that will be displayed in the CMS administration web interface.
+	 * @return Login page link url
+	 */
 	public String getLoginLink();
+	
+	/**
+	 * For authenticated users returns the profile link that will be displayed in the CMS administration web interface.
+	 * @return Profile link url for authenticated users or null for not authenticated requests
+	 */
 	public String getUserProfileLink();
 	
 }
